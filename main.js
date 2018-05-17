ef9b6556a343c4142bd73458517474bc6f5d74ba
chelle tuerk <chelle.tuerk@surveygizmo.com>
2018-05-17 15:09:48 -0600

    change api call from http to https

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $.ajax({
     type: 'GET',
-    url: 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=80301',
+    url: 'https://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=80301',
     success: function(res) {
       console.log(res)
       res.results.forEach(function(e) {
