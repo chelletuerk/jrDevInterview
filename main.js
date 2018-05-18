9f42ef8c3b770d0df04e00b35d9455473a347230
chelle tuerk <chelle.tuerk@surveygizmo.com>
2018-05-17 20:51:52 -0600

    extract marketname and distance from zip code submitted

--- main.js
+++ main.js
@@ -3,15 +3,29 @@ $(document).ready(function() {
     type: 'GET',
     url: 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=80301',
     success: function(res) {
-      console.log(res)
       res.results.forEach(function(res) {
         renderMarketName(res)
       })
+      // onClick(res.target)
     }
   })
 })
 
 
 const renderMarketName = (res) => {
-  return $('.display').append('<li>' + res  .marketname + '</li>')
+  $('.display').append('<li class="market">' + res.marketname + '</li>')
+  // const marketId = res.id
+  // console.log(marketId)
+  onClick(res)
+}
+
+const onClick = (res) => {
+  $('.display').on('click', '.market', (e) => {
+    marketId = res.id
+    marketMiles = res.marketname.split(' ').shift()
+    marketName = res.marketname
+      .split(' ')
+      .splice(1, res.marketname.length)
+      .join(' ')
+  })
 }
