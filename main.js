e75f446ab536d9dac88e72298cf0c0fe217d8b57
chelle tuerk <chelle.tuerk@surveygizmo.com>
2018-05-17 21:48:47 -0600

    assign response to array, move click event into ajax call to pull out of loop

--- main.js
+++ main.js
@@ -1,31 +1,33 @@
+const markets = []
+
 $(document).ready(function() {
   $.ajax({
     type: 'GET',
     url: 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=80301',
     success: function(res) {
-      res.results.forEach(function(res) {
-        renderMarketName(res)
+      markets.push(...res.results)
+      res.results.forEach(function(market) {
+        renderMarketName(market)
       })
-      // onClick(res.target)
+      onClick()
     }
   })
 })
 
-
-const renderMarketName = (res) => {
-  $('.display').append('<li class="market">' + res.marketname + '</li>')
-  // const marketId = res.id
-  // console.log(marketId)
-  onClick(res)
+const renderMarketName = (market) => {
+  const marketName = market.marketname
+    .split(' ')
+    .splice(1, market.marketname.length)
+    .join(' ')
+  $('.display').append('<li class="market">' + marketName + '</li>')
 }
 
-const onClick = (res) => {
-  $('.display').on('click', '.market', (e) => {
-    marketId = res.id
-    marketMiles = res.marketname.split(' ').shift()
-    marketName = res.marketname
-      .split(' ')
-      .splice(1, res.marketname.length)
-      .join(' ')
+const onClick = () => {
+  $('.market').on('click', (e) => {
+    // marketId = res.id
+    // marketMiles = res.marketname.split(' ').shift()
+
+      //$('.popup').append('<div>' + marketMiles + marketName + '</div>')
+      // console.log(e.target)
   })
 }
