b26600f68ac97a7ab225988ca9af5072c3834cba
chelle tuerk <chelle.tuerk@surveygizmo.com>
2018-05-17 22:03:13 -0600

    get market id

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
     url: 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=80301',
     success: function(res) {
       markets.push(...res.results)
-      res.results.forEach(function(market) {
+      console.log(markets)
+      markets.forEach(function(market) {
         renderMarketName(market)
       })
       onClick()
@@ -19,15 +20,11 @@ const renderMarketName = (market) => {
     .split(' ')
     .splice(1, market.marketname.length)
     .join(' ')
-  $('.display').append('<li class="market">' + marketName + '</li>')
+  $('.display').append(`<li id=${market.id} class="market">${marketName}</li>`)
 }
 
 const onClick = () => {
   $('.market').on('click', (e) => {
-    // marketId = res.id
-    // marketMiles = res.marketname.split(' ').shift()
-
-      //$('.popup').append('<div>' + marketMiles + marketName + '</div>')
-      // console.log(e.target)
+    const marketId = e.target.id
   })
 }
